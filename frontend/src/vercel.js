762967148b58ab435951900a2ba8c9caaac82d92
idan <113411952+zidane06-sa@users.noreply.github.com>
2025-12-03 03:14:44 +0700

    Delete Vercel configuration file
    Remove Vercel configuration from vercel.js

--- frontend/src/vercel.js
+++ frontend/src/vercel.js
@@ -1,7 +1,1 @@
-{
-  "buildCommand": "npm install -g vite && npm run build",
-  "devCommand": "npm run dev",
-  "installCommand": "npm install",
-  "outputDirectory": "dist",
-  "framework": "vite"
-}
+
